fbaf7869d313fd8d08ad48275a87868b809e0b0c
Manjushree J <manjushree.j@valtech.com>
2023-10-31 12:11:06 +0530

    commiting

--- first-maven-project/src/main/java/com/valtech/training/first_maven_project/App.java
+++ first-maven-project/src/main/java/com/valtech/training/first_maven_project/App.java
@@ -1,13 +1,17 @@
 package com.valtech.training.first_maven_project;
-
+ 
 /**
- * Hello world!
- *
- */
-public class App 
+* Hello world!
+*
+*/
+public class App
 {
+	public String sayHello() {
+		return "Hello World";
+	}
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
 }
+ 
